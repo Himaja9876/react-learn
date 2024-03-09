1ca3e8902e586126b067a5db299c7cca14a690d0
Himaja9876 <himajachinnam8@gmail.com>
2024-03-09 14:47:14 -0500

    changes

---
+++ App.js
@@ -0,0 +1,26 @@
+//React
+
+/* const heading = React.createElement("h1", {
+  id : "heading", xyz : "abc"
+}, "Hello JS");
+      const root = ReactDOM.createRoot(document.getElementById('root'));
+      root.render(heading); */
+
+      
+
+// Nested div in React
+/*
+<div id = "parent">
+  <div id = "child">
+    <h1>This is grand child</h1>
+    <h2>This is sibling</h2>
+  </div>
+</div> */
+
+const parent = React.createElement("div", {id : "parent"},
+React.createElement("div", {id : "child"}, 
+[React.createElement("h1", {}, "This is grand child"),
+React.createElement("h2", {}, "This is sibling")]))
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(parent);
